056490b0570a123de2e894f894597dc80814aac2
Marco Tortolani <mjtorto@gmail.com>
2024-01-18 18:25:04 -0300

    fix responsive

--- src/components/LabelHeaderPage.jsx
+++ src/components/LabelHeaderPage.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 export default function LabelHeaderPage({ title, lang, setLang }) {
   return (
     <div className=' w-full h-20 mt-0 px-6 pb-2 flex items-end justify-between bg-moobMint rounded-bl-3xl md:rounded-br-3xl'>
-      <h3 className=' pl-0 uppercase text-sm font-poppinsMedium text-white'>{title}</h3>
-      <div className='w-1/6 min-w-14 flex items-center gap-2 '>
+      <h3 className=' pl-0 uppercase text-sm md:text-base lg:text-lg font-poppinsMedium text-white'>{title}</h3>
+      <div className='w-1/6 min-w-14 flex items-center justify-end gap-2'>
         <span className=' font-poppinsMedium'>{lang}</span>
         <div className='w-6'>
           <img
--- src/pages/PortalesTest.jsx
+++ src/pages/PortalesTest.jsx
@@ -8,25 +8,25 @@ export default function PortalesTest() {
   const portalsTestSorted = sortArrayByDate(portalsTest);
 
   return (
-    <main className=' z-0 relative pt-8 md:pt-14 lg:pt-16 w-full md:w-[700px] h-fit overflow-y-hidden font-poppinsLight flex flex-col items-center justify-center '>
+    <main className=' z-0 relative pt-8 md:pt-14 lg:pt-16 w-full max-w-screen-lg  h-fit font-poppinsLight flex flex-col items-center justify-center '>
       <LabelHeaderPage title='Portales Test' lang={lang.es} />
 
       <div className='w-full h-full p-2 pt-4 flex items-center justify-center  content-normal'>
-        <ul className=' w-full h-fit grid place-items-center grid-cols-3 md:grid-cols-4  gap-2 sm:gap-4 md:gap-5'>
+        <ul className=' w-full h-fit grid place-items-center grid-cols-3 md:grid-cols-4  gap-2 sm:gap-3 md:gap-4'>
           {portalsTestSorted.map((el, i) => (
             <li
               key={i}
               onClick={() => {
                 setIndexPag(i);
               }}
-              className={`col-span-1 relative w-full aspect-[5/7] overflow-hidden  `}
+              className={`col-span-1 group hover:scale-110 relative w-full aspect-[5/7] overflow-hidden hover:overflow-visible transition-transform ease-in-out duration-150   `}
             >
               <a
-                className=' w-full h-full pb-1 flex flex-col items-start gap-0'
+                className=' w-full h-full pb-1 flex flex-col items-start gap-0 md:gap-1'
                 href={el.link}
                 target='_blank'
               >
-                <div className='relative w-full h-full flex justify-center  overflow-hidden bg-gray-400 rounded-xl'>
+                <div className='relative w-full h-full flex justify-center  overflow-hidden group-hover:shadow-lg group-hover:shadow-gray-600 transition-all ease-in-out duration-300 rounded-xl'>
                   <img
                     className=' w-full h-full object-cover rounded-xl'
                     src={el.coverImageLogo}
@@ -34,7 +34,7 @@ export default function PortalesTest() {
                   />
                 </div>
 
-                <span className='  font-poppinsLight text-[0.6rem]  line-clamp-1'>
+                <span className='  font-poppinsLight text-[0.6rem] md:text-sm lg:text-base  line-clamp-1'>
                   {el.name}
                 </span>
               </a>
