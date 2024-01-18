aa7328a65b5a5a9b1d5b92c095631d1f775b681a
Marco Tortolani <mjtorto@gmail.com>
2024-01-18 18:39:49 -0300

    fix responsive

--- src/pages/Reels.jsx
+++ src/pages/Reels.jsx
@@ -132,38 +132,38 @@ const lang = { es: 'ES', en: 'GB', us: 'US', pt: 'PT' };
 
 export default function Reels() {
   return (
-    <main className=' z-0 relative pt-8 md:pt-14 lg:pt-16 w-full md:w-[700px] h-fit overflow-y-hidden font-poppinsLight flex flex-col items-center justify-center '>
+    <main className=' z-0 relative pt-8 md:pt-14 lg:pt-16 w-full max-w-screen-lg h-fit font-poppinsLight flex flex-col items-center justify-center '>
       <LabelHeaderPage title='Memoob Video & Reels' lang={lang.es} />
 
       <div className='w-full h-full p-2 pt-4 flex items-center justify-center  content-normal'>
-        <ul className=' w-full h-fit p-0  grid place-items-center  grid-cols-3 md:grid-cols-4  gap-2 sm:gap-4 md:gap-5'>
+        <ul className=' w-full h-fit p-0  grid place-items-center  grid-cols-2 md:grid-cols-3  gap-3 sm:gap-4 md:gap-5'>
           {reelsElements.map((el, i) => (
             <li
               key={i}
               onClick={() => {
                 setIndexPag(i);
               }}
-              className={`col-span-1 relative w-full aspect-[5/7] overflow-hidden   `}
+              className={`col-span-1 relative w-full aspect-[4/3] overflow-hidden group hover:scale-110 hover:overflow-visible transition-transform ease-in-out duration-150  `}
             >
               <a
                 className=' w-full h-full pb-1 flex flex-col items-start gap-0'
                 href={el.link}
                 target='_blank'
               >
-                <div className='relative w-full h-full flex justify-center overflow-hidden rounded-xl'>
+                <div className='relative w-full h-full flex justify-center overflow-hidden group-hover:shadow-lg group-hover:shadow-gray-600 transition-all ease-in-out duration-300 '>
                   <img
-                    className=' w-full h-full object-cover rounded-xl'
+                    className=' w-full h-full object-cover '
                     src={el.coverImageLogo}
                     alt={`${el.name} cover and logo`}
                   />
                   <div className='z-10 absolute top-0 w-full h-full bg-black bg-opacity-50 content-normal' />
 
-                  <div className=' z-20 absolute top-0 w-1/3 h-full flex items-center justify-center '>
+                  <div className=' z-20 absolute top-0 w-1/4 h-full flex items-center justify-center '>
                     <VideoIcon />
                   </div>
                 </div>
 
-                <span className='  font-poppinsLight text-[0.6rem]  line-clamp-1'>
+                <span className='  font-poppinsLight text-[0.6rem] md:text-sm lg:text-base  line-clamp-1'>
                   {el.name}
                 </span>
               </a>
