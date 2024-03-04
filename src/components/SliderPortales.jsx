9721f43509b41c8f17b0542b152fa9411a47d835
Marco Tortolani <mjtorto@gmail.com>
2024-03-04 17:11:56 -0300

    fix height space

--- src/components/SliderPortales.jsx
+++ src/components/SliderPortales.jsx
@@ -34,12 +34,12 @@ export default function SliderPortales({ slides, spaceBetweenSlides = 10 }) {
   }
 
   return (
-    <section className=' z-30 relative top-0 w-screen h-fit lg:px-2 pb-2 md:pb-4 flex items-center justify-center'>
-      <div className='w-full max-w-fit flex flex-col items-start justify-center gap-1 md:gap-2 lg:gap-3 '>
-        <h3 className=' pl-2 uppercase text-sm md:text-base lg:text-lg font-poppinsRegular'>
+    <section className=" z-30 relative top-0 w-screen h-fit lg:px-2 pb-2 md:pb-4 flex items-center justify-center">
+      <div className="w-full max-w-fit flex flex-col items-start justify-center gap-1 md:gap-2 lg:gap-3 ">
+        <h3 className=" pl-2 uppercase text-sm md:text-base lg:text-lg font-poppinsRegular">
           Portales Test
         </h3>
-        <div className='w-full max-w-screen-lg overflow-x-scroll no-scrollbar h-fit content-normal '>
+        <div className="w-full max-w-screen-lg overflow-x-scroll no-scrollbar h-fit  content-normal ">
           <Swiper
             ref={sliderRef}
             slidesPerView={slidesPerView}
@@ -47,35 +47,35 @@ export default function SliderPortales({ slides, spaceBetweenSlides = 10 }) {
             spaceBetween={spaceBetweenSlides}
             modules={[Navigation]}
             navigation={false}
-            className='mySwiper w-full h-full  px-2 overflow-hidden  flex flex-col items-center justify-center gap-2 '
+            className="mySwiper w-full h-full  px-2 overflow-hidden  flex flex-col items-center justify-center gap-2 "
           >
             {slides.map((slide, i) => (
               <SwiperSlide
                 key={i}
                 className={`
-                  w-[40vw] aspect-[3/4] pb-0 overflow-y-hidden `}
+                  w-[40vw] aspect-[3/4] pb-4 md:pb-5 lg:pb-6 overflow-y-hidden `}
               >
                 <a
-                  className='w-full h-full flex flex-col items-start gap-1'
+                  className="w-full h-full flex flex-col items-start gap-1"
                   href={slide.link}
-                  target='_blank'
+                  target="_blank"
                 >
                   <img
-                    className=' w-full h-full object-cover rounded-xl select-none pointer-events-none'
+                    className=" w-full h-full object-cover rounded-xl select-none pointer-events-none"
                     src={slide.coverImageLogo}
                     alt={`${slide.name} cover and logo`}
                   />
                   <FlagBadge lang={slide.lang} />
-                  <span className=' font-poppinsLight text-xs md:text-sm lg:text-base leading-3 line-clamp-1'>
-                    {slide.name}
-                  </span>
                 </a>
+                <span className=" font-poppinsLight text-xs md:text-sm lg:text-base line-clamp-1">
+                  {slide.name}
+                </span>
               </SwiperSlide>
             ))}
           </Swiper>
         </div>
 
-        <div className=' pl-2'>
+        <div className=" pl-2">
           <ButtonSeeMore handleClick={handleClick} />
         </div>
       </div>
